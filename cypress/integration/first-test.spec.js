0fabd9cfac80ff111b4353ec90977e07c2e9ada4
Valentin Yurchenko <yusia260795@gmail.com>
2021-11-17 16:32:13 +0300

    lector about finding elements by Get, Find, Eq

---
+++ cypress/integration/first-test.spec.js
@@ -0,0 +1,48 @@
+/// <reference types = "Cypress" />
+/* 
+it('By ID', () => {
+    cy.visit('https://www.facebook.com/')
+    cy.get('#email')
+});
+
+it('By Class', () => {
+    cy.visit('https://docs.cypress.io/api/commands/focus')
+    cy.get('.DocSearch-Button')
+});
+
+it('By Tag', () => {
+    cy.visit('https://docs.cypress.io/api/commands/focus')
+    cy.get('nav')
+});
+
+it('By Attribute', () => {
+    cy.visit('https://www.facebook.com/')
+    cy.get('[name=pass]')
+});
+
+it('By Different Attributes', () => {
+    cy.visit('https://www.facebook.com/')
+    cy.get('[data-testid="royal_login_button"][value="Вход"]')
+});
+
+it('By Different Types', () => {
+    cy.visit('www.facebook.com')
+    cy.get('a[data-testid="open-registration-form-button"]')
+});
+
+it.only('By Contains Text', () => {
+    cy.visit('www.facebook.com')
+    cy.get('[class^="_42"]')
+});
+ */
+it('Get, Find, Eq', () => {
+    cy.visit("https://next.privat24.ua/deposit/open")
+    cy.get('tbody').find("td").find('div').find('button').eq(8)
+});
+
+it.only('Get, Find, Eq', () => {
+    cy.viewport(1600, 600)
+    cy.visit("https://docs.cypress.io/api/commands/eq#Syntax")
+    cy.get('.w-sidebar').find('ul').find('li').find('a').eq(0)
+});
+
